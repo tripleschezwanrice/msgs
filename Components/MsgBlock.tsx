6f3d109dbeb7afec690983dcc218ced5170ae45d
tripleschezwanrice <varunparmar@jklu.edu.in>
2023-12-29 18:31:09 +0530

    changed: button colors

--- Components/MsgBlock.tsx
+++ Components/MsgBlock.tsx
@@ -3,7 +3,7 @@ import { Message } from "@/Data/data"
 
 export default function MsgBlock({message}:{message:string}){
   return (
-    <div className="w-[33vw] rounded-full bg-yellow-200 p-4 border-2 border-black">
+    <div className="w-[33vw] rounded-full bg-yellow-300 p-4 border-2 border-black">
         {message}
     </div>
   )
